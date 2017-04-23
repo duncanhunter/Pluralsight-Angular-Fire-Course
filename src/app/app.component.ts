b6f0a51594f73ebd9d9e500d4def0299bf364851
Duncan Hunter <duncan@duncanhunter.com.au>
2017-04-23 18:55:25 +1000

    after - m2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  constructor(private db: AngularFireDatabase){
+    this.db.object(`connected`)
+      .subscribe(console.log);
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBJsTnYKiu2mdLnwkSidQjw3THCty5sEm4',
+  authDomain: 'buildingappswithangularf-ebcb5.firebaseapp.com',
+  databaseURL: 'https://buildingappswithangularf-ebcb5.firebaseio.com',
+  projectId: 'buildingappswithangularf-ebcb5',
+  storageBucket: 'buildingappswithangularf-ebcb5.appspot.com',
+  messagingSenderId: '957445559070'
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +23,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
