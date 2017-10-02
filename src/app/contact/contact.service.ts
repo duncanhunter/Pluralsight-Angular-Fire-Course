2e7f01c94949e70191b75efd6ff898a052ad4af5
Duncan Hunter <duncan@duncanhunter.com.au>
2017-10-02 22:36:00 +1000

    update material and add MdSelectModule

--- src/app/contact/contact.service.ts
+++ src/app/contact/contact.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/from';
 import 'rxjs/add/observable/combineLatest';
 import 'rxjs/add/operator/catch';
